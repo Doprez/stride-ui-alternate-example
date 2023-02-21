cadbe80c379b5a3f896fca9a4c7c224976468410
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-20 18:46:03 -0700

    created script to update UI at runtime

---
+++ stride_ui_alternate_example/RandomizeHealthbar.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.Core;
+using stride_ui_alternate_example.Views;
+
+namespace stride_ui_alternate_example
+{
+    public class RandomizeHealthbar : SyncScript
+    {
+
+        private Random _random;
+        private MainView _mainView;
+
+        public override void Start()
+        {
+			_random = new Random();
+            Game.Services.ServiceAdded += ServicesChanged;
+        }
+
+        private void ServicesChanged(object sender, ServiceEventArgs e)
+        {
+            if(e.ServiceType == typeof(MainView))
+            {
+                _mainView = Game.Services.GetService<MainView>();
+            }
+        }
+
+        public override void Update()
+        {
+            if (Input.IsKeyDown(Keys.Space))
+            {
+                _mainView.HealthBar.Value = _random.Next(0, 100);
+            }
+        }
+    }
+}
