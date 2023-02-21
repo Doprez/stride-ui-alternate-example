a4e917384f9374c3b3355382929f1fa799a9bd16
Doprez <73259914+Doprez@users.noreply.github.com>
2023-02-20 20:39:46 -0700

    cleaned up project to make things more readable

---
+++ stride_ui_alternate_example/HealthRandomizer.cs
@@ -0,0 +1,40 @@
+﻿using Stride.Core.Shaders.Ast;
+using Stride.Engine;
+using Stride.Input;
+using stride_ui_alternate_example.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stride_ui_alternate_example;
+public class HealthRandomizer : SyncScript
+{
+
+	private MainView _mainView;
+	private Random _random;
+
+	public override void Start()
+	{
+		base.Start();
+		Game.Services.ServiceAdded += ServiceAdded;
+		_random = new Random();
+	}
+
+	private void ServiceAdded(object sender, Stride.Core.ServiceEventArgs e)
+	{
+		if(e.ServiceType == typeof(MainView))
+		{
+			_mainView = Game.Services.GetService<MainView>();
+		}
+	}
+
+	public override void Update()
+	{
+		if (Input.IsKeyDown(Keys.Space))
+		{
+			_mainView.HealthBar.Value = _random.Next(0, 100);
+		}
+	}
+}
--- stride_ui_alternate_example/Views/MainView.cs
+++ stride_ui_alternate_example/Views/MainView.cs
@@ -4,30 +4,23 @@ using Myra.Graphics2D.UI;
 namespace stride_ui_alternate_example.Views;
 public class MainView : Panel
 {
-
 	public HorizontalProgressBar HealthBar;
 
-	public MainView() 
+	public MainView()
 	{
-		var crosshair = new Label();
-		crosshair.Text = ".";
-		crosshair.HorizontalAlignment = HorizontalAlignment.Center;
-		crosshair.VerticalAlignment = VerticalAlignment.Center;
+		HealthBar = new()
+		{
+			HorizontalAlignment = HorizontalAlignment.Left,
+			VerticalAlignment = VerticalAlignment.Bottom,
+			Value = 100,
+			Filler = new SolidBrush("#4BD961FF"),
+			Left = 20,
+			Top = -20,
+			Width = 300,
+			Height = 20,
+			Background = new SolidBrush("#202020FF"),
+		};
 
-		HealthBar = new HorizontalProgressBar();
-		HealthBar.HorizontalAlignment = HorizontalAlignment.Left;
-		HealthBar.VerticalAlignment = VerticalAlignment.Bottom;
-		HealthBar.Value = 50;
-		HealthBar.Filler = new SolidBrush("#4BD961FF");
-		HealthBar.Left = 20;
-		HealthBar.Top = -20;
-		HealthBar.Width = 300;
-		HealthBar.Height = 50;
-		HealthBar.Background = new SolidBrush("#202020FF");
-		HealthBar.Id = "Health Bar";
-
-		Widgets.Add(crosshair);
 		Widgets.Add(HealthBar);
-
 	}
 }
